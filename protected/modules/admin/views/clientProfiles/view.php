d3065c2575a050c6d77404a95ac96d61e2e1695e
deveshjadon <devesh.jadon@venturepact.com>
2015-02-19 16:51:18 +0530

    client changes

--- protected/modules/admin/views/clientProfiles/view.php
+++ protected/modules/admin/views/clientProfiles/view.php
@@ -16,6 +16,7 @@ $this->menu=array(
 );
 ?>
 
+<br><br>
 <div class="box inverse">
         <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xs-offset-0 col-sm-offset-0 col-md-offset-3 col-lg-offset-3 toppad" >
    			<div class="panel panel-info">
