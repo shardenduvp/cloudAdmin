6686e8bb061f669552d120705061497c8e0c5ffc
samridhi25 <sam25aug@gmail.com>
2015-02-19 13:06:43 +0530

    Modified Suppliers Table

--- protected/modules/admin/views/suppliersPortfolioHasIndustries/admin.php
+++ protected/modules/admin/views/suppliersPortfolioHasIndustries/admin.php
@@ -40,8 +40,23 @@ or <b>=</b>) at the beginning of each of your search values to specify how the c
 )); ?>
 </div><!-- search-form -->
 
+<div class="row">
+	<div class="col-md-12">
+		<!-- BOX -->
+		<div class="box border blue">
+
+			<div class="box-title">
+				<h4><i class="fa fa-table"></i>List of suppliersPortfolioHasIndustries</h4>
+			</div>
+									
+
+			<div class="box-body">
+
 <?php $this->widget('zii.widgets.grid.CGridView', array(
-	'id'=>'suppliers-portfolio-has-industries-grid',
+	//'id'=>'suppliers-portfolio-has-industries-grid',
+	'id'=>'datatables1',
+					'itemsCssClass'=>'datatable table table-striped table-bordered table-hover',
+
 	'dataProvider'=>$model->search(),
 	'filter'=>$model,
 	'columns'=>array(
@@ -52,6 +67,23 @@ or <b>=</b>) at the beginning of each of your search values to specify how the c
 		'status',
 		array(
 			'class'=>'CButtonColumn',
+			'header'=>'Operations',
+							'buttons'=>array(
+                                        'update'=>array(
+                                                        'visible'=>'true',
+                                                ),
+                                        'view'=>array(
+                                                        'visible'=>'true',
+                                                ),
+                                        'delete'=>array(
+                                                        'visible'=>'false',
+                                                ),
+                       						 )
 		),
 	),
 )); ?>
+</div>
+		</div>
+	<!-- /BOX -->
+	</div>
+</div>
