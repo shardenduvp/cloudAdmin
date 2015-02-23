a6af1f31f8bb35ec7943fd0710624c80fe00d546
deveshjadon <devesh.jadon@venturepact.com>
2015-02-23 13:35:22 +0530

    user view

--- protected/modules/admin/views/users/view.php
+++ protected/modules/admin/views/users/view.php
@@ -18,112 +18,344 @@ $this->menu=array(
 
 <br/><br/>
 <div class="box">
-        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xs-offset-0 col-sm-offset-0 col-md-offset-3 col-lg-offset-3 toppad" >
-   			<div class="panel panel-info">
-            	<div class="panel-heading ">
-              		<h3 class="panel-title"><b>User ID #<?php echo $model->id; ?></b></h3>
-              	</div>
-        <div class="panel-body">
-            <div class="row">
-              	<div class="col-md-3 col-lg-3 " align="center"> 
-              		<img alt="User Pic" class="img-circle" style="max-width:100px;" src="<?php 
-              									if($model->image==null){
-													echo Yii::app()->theme->baseUrl."/img/user.png";
-              									}
-              									else{
-              										echo $model->image;
-              									}
-					?>" > 
-              	</div>
-                <div class=" col-md-9 col-lg-9 "> 
-                  	<table class="table table-user-information">
-                    	<tbody>
-							
-							<?php
-                    			$attributes=array(
-												 	'last_name',
-												  	'first_name',
-													'company_name',
-													'display_name',
-													'username',
-													'phone_number',
-													'password',
-													'linkedin',
-													'add_date',
-													'last_login',
-													'status',
-													'role_id',
-												);
-                    			foreach($attributes as $attr) {
-                    				$value=$model->$attr;
-                    				//if password display same number of stars instead
-                    				if($attr=='password'){
-                    					$value=base64_decode($model->$attr);
-                    				}
-                    				//according to role_id display role_name
-                    				else if($attr=='role_id'){
-                    					switch ($model->$attr)
-                    					{
-                    						case 1:
-                    							$value='Admin';
-                    							break;
-                    						case 2:
-                    							$value='Client';
-                    							break;
-                    						default:
-                    							$value='Supplier';
-                    					}
-                    					$attr='Role';
-                    				}
-                    				//according to status value display status code
-                    				else if($attr=='status'){
-                    					if($model->$attr==1)
-                    							$value='Verified';
-                    					else
-                    							$value='Not-Verified';
-                    					
-                    					$attr='Verification-Status';
-                    				}
-         					?>    
-       
-         					<tr>
-         						<td><b><?php echo ucfirst($attr); ?></b></td>
-         						<td><?php echo $value; ?></td>
-         						
-         					</tr> 
-
-         					<?php }?>
-
-                        </tbody>
-                  	</table>
-                </div>
-            </div>
-        </div>
-              
+  <div>
+   	<div class="panel panel-info">
+      <div class="panel-heading ">
+        <h3 class="panel-title"><b>User Details For Id #<?php echo $model->id; ?></b></h3>
+      </div>
+      <div class="panel-body">
+        <div class="row">
+          <div class="col-lg-2 " align="center"> 
+            <img alt="User Pic" class="img-circle" style="max-width:100px;" src="<?php if($model->image==null){
+                                                                                        echo Yii::app()->theme->baseUrl."/img/user.png";
+              									                                                       }
+              									                                                       else{
+                                                                                        echo $model->image;
+              									                                                       }
+					                                                                       ?>" > 
           </div>
+          <div class=" col-md-9 col-lg-10 "> 
+            <table class="table table-user-information">
+              <tbody><?php
+                    		$attributes=array(
+												                    'last_name',
+											                      'first_name',
+													                  'company_name',
+													                  'display_name',
+													                  'username',
+													                  'phone_number',
+													                  'password',
+													                  'linkedin',
+													                  'add_date',
+													                  'last_login',
+													                  'status',
+													                  'role_id',
+												            );
+                    	 	foreach($attributes as $attr) {
+                    			$value=$model->$attr;
+                    			//if password display same number of stars instead
+                    			if($attr=='password'){
+                    				$value=base64_decode($model->$attr);
+                    			}
+                    			//according to role_id display role_name
+                    			else if($attr=='role_id'){
+                    				switch ($model->$attr)
+                    				{
+                    					case 1:
+                    						$value='Admin';
+                    						break;
+                    					case 2:
+                    						$value='Client';
+                    						break;
+                    					default:
+                    						$value='Supplier';
+                    				}
+                    				$attr='Role';
+                    			}
+                    			//according to status value display status code
+                    			else if($attr=='status'){
+                    				if($model->$attr==1)
+                    					$value='Verified';
+                    				else
+                    					$value='Not-Verified';
+                    				$attr='Verification-Status';
+                    			}?>    
+       
+         				<tr>
+         					<td><b><?php echo ucfirst($attr); ?></b></td>
+         					<td><?php echo $value; ?></td>	
+         				</tr> 
+         				<?php }
+                ?>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>        
+    </div>
+  </div>
+</div>
+
+<?php if(!empty($model->clientProfiles)){?>
+    <?php foreach($model->clientProfiles as $var){?>
+    <div class="box">
+      <div>
+        <div class="panel panel-info">
+          <div class="panel-heading ">
+            <h3 class="panel-title"><b>Client Details For User Id #<?php echo $var->users_id; ?></b></h3>
+          </div>
+          <div class="panel-body">
+            <div class="row">
+              <div class=" col-md-9 col-lg-10 "> 
+                <table class="table table-user-information">
+                  <tbody>   
+                    <tr>
+                      <td><b>Id</b></td>
+                      <td><?php echo $var->id; ?></td> 
+                    </tr>
+                    <tr>
+                      <td><b>First Name</b></td>
+                      <td><?php echo $var->first_name; ?></td> 
+                    </tr> 
+                    <tr>
+                      <td><b>Last Name?></b></td>
+                      <td><?php echo $var->last_name; ?></td> 
+                    </tr> 
+                    <tr>
+                      <td><b>Company Link</b></td>
+                      <td><?php echo $var->company_link; ?></td> 
+                    </tr> 
+                    <tr>
+                      <td><b>Skype Id</b></td>
+                      <td><?php echo $var->skype_id; ?></td> 
+                    </tr> 
+                    <tr>
+                      <td><b>Email</b></td>
+                      <td><?php echo $var->email; ?></td> 
+                    </tr> 
+                    <tr>
+                      <td><b>Address</b></td>
+                      <td><?php echo $var->address1; ?></td> 
+                    </tr> 
+                    <tr>
+                      <td><b>Team Size</b></td>
+                      <td><?php echo $var->team_size; ?></td> 
+                    </tr> 
+                    <tr>
+                      <td><b>Category</b></td>
+                      <td><?php echo $var->category; ?></td> 
+                    </tr> 
+                    <tr>
+                      <td><b>Foundation Year</b></td>
+                      <td><?php echo $var->foundation_year; ?></td> 
+                    </tr> 
+                    <tr>
+                      <td><b>Description</b></td>
+                      <td><?php echo $var->description; ?></td> 
+                    </tr> 
+                    <tr>
+                      <td><b>Add Date</b></td>
+                      <td><?php echo $var->add_date; ?></td> 
+                    </tr> 
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>    
         </div>
       </div>
     </div>
-
 <?php 
-//$this->widget('zii.widgets.CDetailView', array(
-// 	'data'=>$model,
-// 	'attributes'=>array(
-// 		'id',
-// 		'last_name',
-// 		'first_name',
-// 		'image',
-// 		'company_name',
-// 		'display_name',
-// 		'username',
-// 		'phone_number',
-// 		'password',
-// 		'linkedin',
-// 		'role',
-// 		'add_date',
-// 		'last_login',
-// 		'status',
-// 		'role_id',
-// 	),
-// ));
- ?>
+    }
+  }
+  if(!empty($model->suppliers)){
+    foreach($model->suppliers as $var){?>
+    <div class="box">
+      <div>
+        <div class="panel panel-info">
+          <div class="panel-heading ">
+            <h3 class="panel-title"><b>Supplier Details For User Id #<?php echo $var->users_id; ?></b></h3>
+          </div>
+          <div class="panel-body">
+            <div class="row">
+              <div class=" col-md-9 col-lg-10 "> 
+                <table class="table table-user-information">
+                  <tbody>  
+                    <tr>
+                      <td><b>Id</b></td>
+                      <td><?php echo $var->id; ?></td> 
+                    </tr> 
+                    <tr>
+                      <td><b>Name</b></td>
+                      <td><?php echo $var->name; ?></td> 
+                    </tr> 
+                    <tr>
+                      <td><b>Cover</b></td>
+                      <td><?php echo $var->cover; ?></td> 
+                    </tr> 
+                    <tr>
+                      <td><b>Website</b></td>
+                      <td><?php echo $var->website; ?></td> 
+                    </tr> 
+                    <tr>
+                      <td><b>Skype Id</b></td>
+                      <td><?php echo $var->skype_id; ?></td> 
+                    </tr> 
+                    <tr>
+                      <td><b>Email</b></td>
+                      <td><?php echo $var->email; ?></td> 
+                    </tr> 
+                    <tr>
+                      <td><b>Tagline</b></td>
+                      <td><?php echo $var->tagline; ?></td> 
+                    </tr> 
+                    <tr>
+                      <td><b>About Company</b></td>
+                      <td><?php echo $var->about_company; ?></td> 
+                    </tr> 
+                    <tr>
+                      <td><b>Short Desc</b></td>
+                      <td><?php echo $var->short_description; ?></td> 
+                    </tr> 
+                    <tr>
+                      <td><b>Details</b></td>
+                      <td><?php echo $var->details; ?></td> 
+                    </tr> 
+                    <tr>
+                      <td><b>Foundation Year</b></td>
+                      <td><?php echo $var->foundation_year; ?></td> 
+                    </tr> 
+                    <tr>
+                      <td><b>Location</b></td>
+                      <td><?php echo $var->location; ?></td> 
+                    </tr> 
+                    <tr>
+                      <td><b>Pricing Details</b></td>
+                      <td><?php echo $var->pricing_details; ?></td> 
+                    </tr>
+                    <tr>
+                      <td><b>Minimum Price</b></td>
+                      <td><?php echo $var->min_price; ?></td> 
+                    </tr>
+                    <tr>
+                      <td><b>Number Of Employee</b></td>
+                      <td><?php echo $var->number_of_employee; ?></td> 
+                    </tr>
+                    <tr>
+                      <td><b>Total Available Employee</b></td>
+                      <td><?php echo $var->total_available_employee; ?></td> 
+                    </tr>
+                    <tr>
+                      <td><b>Consultation Description</b></td>
+                      <td><?php echo $var->consultation_description; ?></td> 
+                    </tr> 
+                    <tr>
+                      <td><b>Standard Pitch</b></td>
+                      <td><?php echo $var->standard_pitch; ?></td> 
+                    </tr>     
+                    <tr>
+                      <td><b>Standard Service Agreement</b></td>
+                      <td><?php echo $var->standard_service_agreement; ?></td> 
+                    </tr> 
+                    <tr>
+                      <td><b>Profile Status</b></td>
+                      <td><?php echo $var->profile_status; ?></td> 
+                    </tr> 
+                    <tr>
+                      <td><b>Add Date</b></td>
+                      <td><?php echo $var->add_date; ?></td> 
+                    </tr> 
+                    <tr>
+                      <td><b>Modification Date</b></td>
+                      <td><?php echo $var->modification_date; ?></td> 
+                    </tr> 
+                    <tr>
+                      <td><b>Rough Estimate</b></td>
+                      <td><?php echo $var->rough_estimate; ?></td> 
+                    </tr> 
+                    <tr>
+                      <td><b>Linked In</b></td>
+                      <td><?php echo $var->linkedin; ?></td> 
+                    </tr> 
+                    <tr>
+                      <td><b>Facebook</b></td>
+                      <td><?php echo $var->facebook; ?></td> 
+                    </tr> 
+                    <tr>
+                      <td><b>Google</b></td>
+                      <td><?php echo $var->google; ?></td> 
+                    </tr>
+                    <tr>
+                      <td><b>Twitter</b></td>
+                      <td><?php echo $var->twitter; ?></td> 
+                    </tr>  
+                    <tr>
+                      <td><b>Youtube</b></td>
+                      <td><?php echo $var->you_tube; ?></td> 
+                    </tr> 
+                    <tr>
+                      <td><b>Per Hour Rate</b></td>
+                      <td><?php echo $var->per_hour_rate; ?></td> 
+                    </tr> 
+                    <tr>
+                      <td><b>Project Size</b></td>
+                      <td><?php echo $var->project_size; ?></td> 
+                    </tr> 
+                    <tr>
+                      <td><b>Web References</b></td>
+                      <td><?php echo $var->web_references; ?></td> 
+                    </tr>
+                    <tr>
+                      <td><b>Development Location</b></td>
+                      <td><?php echo $var->development_location; ?></td> 
+                    </tr>  
+                    <tr>
+                      <td><b>Sales Location</b></td>
+                      <td><?php echo $var->sales_location; ?></td> 
+                    </tr> 
+                    <tr>
+                      <td><b>Response Time</b></td>
+                      <td><?php echo $var->response_time; ?></td> 
+                    </tr> 
+                    <tr>
+                      <td><b>Status</b></td>
+                      <td><?php echo $var->status; ?></td> 
+                    </tr> 
+                    <tr>
+                      <td><b>Accept New Project Date</b></td>
+                      <td><?php echo $var->accept_new_project_date; ?></td> 
+                    </tr> 
+                    <tr>
+                      <td><b>Link Status</b></td>
+                      <td><?php echo $var->link_status; ?></td> 
+                    </tr> 
+                    <tr>
+                      <td><b>Is Profile Complete</b></td>
+                      <td><?php echo $var->is_profile_complete; ?></td> 
+                    </tr> 
+                    <tr>
+                      <td><b>Offers</b></td>
+                      <td><?php echo $var->offers; ?></td> 
+                    </tr> 
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>      
+        </div>
+      </div>
+    </div>
+<?php   }
+    }
+    if(empty($model->clientProfiles) && empty($model->suppliers)){?>
+        <div class="box">
+          <div>
+            <div class="panel panel-info">
+              <div class="panel-heading ">
+                  <h3 class="panel-title">Neither a client nor a supplier</b></h3>
+              </div>
+            </div>
+          </div>
+        </div>
+ <?php   }?>
+
