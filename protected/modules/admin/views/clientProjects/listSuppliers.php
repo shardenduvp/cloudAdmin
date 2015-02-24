919bae9e8157c132cd0b49182e79f8749bed7839
VaishaliYadav <vaishali.yadav@venturepact.com>
2015-02-24 10:30:22 +0530

    changes in the new leads

--- protected/modules/admin/views/clientProjects/listSuppliers.php
+++ protected/modules/admin/views/clientProjects/listSuppliers.php
@@ -1,25 +1,26 @@
 <br>
-<br><div class="box">
-	<div class="panel panel-info">
-            	<div class="panel-heading ">
+<br><div class="box panelColor ">
+	<div class="panel borderleft">
+            	<div class="panel-heading panelBackgrnd paneltext">
             	 <h3 class="panel-title"><b>Project Name:</b><?php echo $model_client->name; ?></h3> 
 			            	
 			     </div>
                   
                   <?php foreach($model as $projSupp) { ?>
                   <div class="row">
-	                  <div class="col-xs-1 col-xs-offset-1">
+	                  <div class="col-xs-1 col-xs-offset-1 align">
 	                  	<div class="row">
+	                  	<h1></h1>
 	                  		<i class="fa fa-users fntsize  "></i>
 	                  		</div>
-	                  		<div class="row">
+	                  		<div class="row align">
 	                  		Introduction Sent
 	                  		</div>
 	                  	</div>
 	                
-	                  <div class="col-xs-5"> 
+	                  <div class="col-xs-5 verticalLine"> 
 
-	                  	<h3><?php echo $projSupp->suppliers->name; ?></h3>
+	                  	<h3><b><?php echo $projSupp->suppliers->name; ?></b></h3>
 	                  	<div class="row">
 		                  	<div class="col-xs-5">
 		                  		<i class="fa fa-map-marker "></i> 
@@ -43,27 +44,33 @@
 		                  	</div>
 		                 </div>
 		                </div>
-		                <div class="col-xs-2">
-				                <div class="row col-xs-offset-1">
+
+
+		                <div class="col-xs-1 verticalLine">
+		               			 
+				                <div class="row align">
 				                <h3>3/5</h3>
 				                </div>	
-				                <div class="row">
+				                <div class="row align">
 				                Relevant Client Stories
 				                </div>	
 		                </div>
-		                <div class="col-xs-2">
-				                <div class="row col-xs-offset-1">
+		                <div class="col-xs-2 verticalLine">
+				                <div class="row align">
 				                <h3>$<?php echo $projSupp->min_price; ?>-<?php echo $projSupp->max_price; ?></h3>
 				                </div>	
-				                <div class="row">
-				                Average Per Hour (in USD)
+				                <div class="row align">
+				               Average Per Hour <br> (in USD)
 				                </div>	
 		                </div>
 		                <div class="col-xs-1">
-				                <div class="row">
-				                <h3></h3>
+				                <div class="row align col-xs-offset-1">
+				                <h2></h2>
+				                	<div class="box1 boxcolor align">
+				                	3.8
+				                	</div>
 				                </div>	
-				                <div class="row">
+				                <div class="row align">
 				                27 Reviews
 				                </div>	
 		                </div>
