5471c23f167645bc55d392927b31d906e5d8d50f
shardenduvp <shardendu.kumar@venturepact.com>
2015-02-26 16:32:35 +0530

    Advance Search Design

--- protected/modules/admin/views/default/_search.php
+++ protected/modules/admin/views/default/_search.php
@@ -27,123 +27,145 @@
 
 
 				<div class="form-group">
-					<label class="control-label col-sm-2">
-					<?php echo $form->labelEx($model,'name'); ?></label>
-					<div class="col-sm-4">
+					<div class="col-sm-4 tr-align">
+						<?php echo $form->labelEx($model,'name', array(
+							'class'=>'control-label'
+						)); ?>
+					</div>
+					<div class="col-sm-6 col-offset-sm-2">
 					<?php echo $form->textField($model,'name',array('size'=>52,'maxlength'=>100,'class'=>'form-control')); ?>
 					</div>
 				</div>
 
-
-
-
-					<div class="form-group">
-						<label class="control-label col-md-2">
-						<?php echo $form->label($model,'team_size'); ?></label>
-		
-						<div class="col-sm-4">
+				<div class="form-group">
+					<div class="col-sm-4 tr-align">
+						<?php echo $form->label($model,'team_size', array(
+							'class'=>'control-label'   
+						)); ?>
+					</div>
+					<div class="col-sm-6 col-offset-sm-2">
 						<?php echo $form->textField($model,'team_size',array('size'=>52,'maxlength'=>100,'class'=>'form-control')); ?>
-						</div>
 					</div>
+				</div>
   
-					<div class="form-group">
-							<label class="control-label col-sm-2">
-							<?php echo $form->label($model,'min_budget'); ?></label>
-						<div class="col-sm-4">
-							<?php echo $form->textField($model,'min_budget',array('size'=>52,'maxlength'=>100,'class'=>'form-control')); ?>
-						</div>
+				<div class="form-group">
+					<div class="col-sm-4 tr-align">
+						<?php echo $form->label($model,'min_budget', array(
+							'class'=>'control-label'   
+						)); ?>
 					</div>
+					<div class="col-sm-6 col-offset-sm-2">
+						<?php echo $form->textField($model,'min_budget',array('size'=>52,'maxlength'=>100,'class'=>'form-control')); ?>
+					</div>
+				</div>
 
-					<div class="form-group">
-						<label class="control-label col-sm-2">
-						<?php echo $form->label($model,'max_budget'); ?></label>
+				<div class="form-group">
+					<div class="col-sm-4 tr-align">
+						<?php echo $form->label($model,'max_budget', array(
+							'class'=>'control-label'   
+						)); ?>
+					</div>
 						
-						<div class="col-sm-4">
+					<div class="col-sm-6 col-offset-sm-2">
 						<?php echo $form->textField($model,'max_budget',array('size'=>52,'maxlength'=>100,'class'=>'form-control')); ?>
-						</div>
 					</div>
+				</div>
 
-
-
-					<div class="form-group">
-						<label class="control-label col-sm-2">
-						<?php echo $form->label($model,'start_date'); ?></label>
-						<div class="col-sm-4">
-						<?php // echo $form->textField($model,'add_date');
-								 $form->widget('zii.widgets.jui.CJuiDatePicker', array(
-									    'model' => $model,
-										    'attribute' => 'start_date',
-								    'htmlOptions' => array(
-									        'size' => '52',         // textField size
-									        'maxlength' => '100' ,
-									        'class'=>'form-control'   // textField maxlength
-								    ),
-									));
-										 ?>
-
-										<?php echo $form->error($model,'start_date'); ?>
-						</div>			
+				<div class="form-group">
+					<div class="col-sm-4 tr-align">
+						<?php echo $form->label($model,'start_date', array(
+							'class'=>'control-label'   
+						)); ?>
 					</div>
+					<div class="col-sm-6 col-offset-sm-2">
+						<?php
+							$form->widget('zii.widgets.jui.CJuiDatePicker', array(
+								'model' => $model,
+								'attribute' => 'start_date',
+								'htmlOptions' => array(
+									'size' => '52',         // textField size
+									'maxlength' => '100' ,
+									'class'=>'form-control'   // textField maxlength
+								),
+							));
+						?>
 
-	
+						<?php echo $form->error($model,'start_date'); ?>
+					</div>			
+				</div>
 
-						<div class="form-group">
-							<label class="control-label col-sm-2">
-							<?php echo $form->label($model,'add_date'); ?></label>
-							<div class="col-sm-4">
-							<?php // echo $form->textField($model,'add_date');
-									 $form->widget('zii.widgets.jui.CJuiDatePicker', array(
-										    'model' => $model,
-											    'attribute' => 'add_date',
-									    'htmlOptions' => array(
-										        'size' => '52',         // textField size
-										        'maxlength' => '100', 
-										        'class'=>'form-control'   // textField maxlength
-									    ),
-										));
-											 ?>
+				<div class="form-group">
+					<div class="col-sm-4 tr-align">
+						<?php echo $form->label($model,'add_date', array(
+							'class'=>'control-label'   
+						)); ?>
+					</div>
+					<div class="col-sm-6 col-offset-sm-2">
+						<?php
+							$form->widget('zii.widgets.jui.CJuiDatePicker', array(
+								'model' => $model,
+								'attribute' => 'add_date',
+								'htmlOptions' => array(
+									'size' => '52',         // textField size
+									'maxlength' => '100', 
+									'class'=>'form-control'   // textField maxlength
+								),
+							));
+						?>
 
-											<?php echo $form->error($model,'add_date'); ?>
-							</div>			
-						</div>
+						<?php echo $form->error($model,'add_date'); ?>
+					</div>			
+				</div>
 
-						<div class="form-group">
-							<label class="control-label col-sm-2">
-							<?php echo $form->label($model,'modify_date'); ?></label>
-							<div class="col-sm-4">
-							<?php // echo $form->textField($model,'add_date');
-									 $form->widget('zii.widgets.jui.CJuiDatePicker', array(
-										    'model' => $model,
-											    'attribute' => 'modify_date',
-									    'htmlOptions' => array(
-										        'size' => '52',         // textField size
-										        'maxlength' => '100', 
-										        'class'=>'form-control'   // textField maxlength
-									    ),
-										));
-											 ?>
+				<div class="form-group">
+					<div class="col-sm-4 tr-align">
+						<?php echo $form->label($model,'modify_date', array(
+							'class'=>'control-label'   
+						)); ?>
+					</div>
+					<div class="col-sm-6 col-offset-sm-2">
+						<?php
+							$form->widget('zii.widgets.jui.CJuiDatePicker', array(
+								'model' => $model,
+								'attribute' => 'modify_date',
+								'htmlOptions' => array(
+									'size' => '52',         // textField size
+									'maxlength' => '100', 
+									'class'=>'form-control'   // textField maxlength
+								),
+							));
+						?>
 
-											<?php echo $form->error($model,'modify_date'); ?>
+						<?php echo $form->error($model,'modify_date'); ?>
 										
-						      </div>
-						</div>
+					</div>
+				</div>
 
-
-						<div class="form-group">
-							<label class="control-label col-sm-2">
-							<?php echo $form->label($model,'status'); ?></label>
-							<div class="col-sm-4">
-					        <?php //echo $form->textField($model,'role_id'); 
-									echo CHtml::activeDropDownList($model, 'status',
-					                 array('1'=>'Awaiting Approval','2'=>'Introductions Sent'),
-					                array('empty'=>'Select Status',"",'class'=>'form-control'));
-									?>
-							</div>
-						</div>
+				<div class="form-group">
+					<div class="col-sm-4 tr-align">
+							<?php echo $form->label($model,'status', array(
+							'class'=>'control-label'   
+						)); ?>
+					</div>
+					<div class="col-sm-6 col-offset-sm-2">
+					    <?php //echo $form->textField($model,'role_id'); 
+							echo CHtml::activeDropDownList($model, 'status', array(
+								    '1'=>'Awaiting Approval','2'=>'Introductions Sent'
+								), array(
+									'empty'=>'Select Status',
+									"",
+									'class'=>'form-control'
+								)
+							);
+						?>
+					</div>
+				</div>
 	
-	                   <div class="row buttons">
+	            <div class="row">
+	            	<div class="col-sm-4 tr-align"></div>
+	            	<div class="col-sm-6 col-offset-sm-2 search-button">
 						<?php echo CHtml::submitButton('Search',array('class'=>'btn btn-primary')); ?>
-						</div>
+					</div>
 				</div>
 			<?php $this->endWidget(); ?>
 		</div>
