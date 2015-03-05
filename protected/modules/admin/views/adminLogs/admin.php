d918c5ca46e97ca5e10dde82bd3b3339250f3444
samridhi25 <sam25aug@gmail.com>
2015-03-05 15:56:14 +0530

    Admin Logs

--- protected/modules/admin/views/adminLogs/admin.php
+++ protected/modules/admin/views/adminLogs/admin.php
@@ -13,55 +13,122 @@ $this->menu=array(
 );
 
 Yii::app()->clientScript->registerScript('search', "
-$('.search-button').click(function(){
-	$('.search-form').toggle();
-	return false;
-});
+
 $('.search-form form').submit(function(){
-	$('#admin-logs-grid').yiiGridView('update', {
+	
+	var operator=$('.operatorID').val();
+	var val=$('.IDUser').val();
+	if (val!='') {
+		if(val.indexOf('<')!=-1 || val.indexOf('>')!=-1){
+		val=$('.IDUser').val().substr(1);
+		}
+		if(val.indexOf('<')!=-1 || val.indexOf('=')!=-1 ||val.indexOf('>')!=-1){
+			val=val.substr(1);
+		}
+		$('.IDUser').val(operator+val);
+	}
+	
+
+	var operatorDate=$('.operatorIDforDate').val();
+	var val1=$('.add_dateUSER').val();
+
+	if(val1.indexOf('<')!=-1 || val1.indexOf('>')!=-1){
+		val1=$('.add_dateUSER').val().substr(1);
+	}
+	if(val1.indexOf('<')!=-1 || val1.indexOf('=')!=-1 ||val1.indexOf('>')!=-1){
+		val1=val1.substr(1);
+	}
+	
+	$('.add_dateUSER').val(operatorDate+val1);
+
+
+	$('#datatables1').yiiGridView('update', {
 		data: $(this).serialize()
 	});
 	return false;
 });
 ");
 ?>
+<div class="row">
+    <div class="col-sm-12">
+        <div class="page-header">
+         	<h1>Manage Admin Logs</h1>
+       	</div>
+    </div>
+</div>
 
-<h1>Manage Admin Logs</h1>
+
 
 <p>
 You may optionally enter a comparison operator (<b>&lt;</b>, <b>&lt;=</b>, <b>&gt;</b>, <b>&gt;=</b>, <b>&lt;&gt;</b>
 or <b>=</b>) at the beginning of each of your search values to specify how the comparison should be done.
 </p>
 
-<?php echo CHtml::link('Advanced Search','#',array('class'=>'search-button')); ?>
-<div class="search-form" style="display:none">
+<div class="search-form">
 <?php $this->renderPartial('_search',array(
 	'model'=>$model,
 )); ?>
 </div><!-- search-form -->
 
-<?php $this->widget('zii.widgets.grid.CGridView', array(
-	'id'=>'admin-logs-grid',
-	'dataProvider'=>$model->search(),
-	'filter'=>$model,
-	'columns'=>array(
-		'id',
-		'username',
-		'ipaddress',
-		'logtime',
-		'controller',
-		'action',
-		/*
-		'details',
-		'action_param',
-		'browser',
-		'query_string',
-		'refer_url',
-		'user_id',
-		'request_url',
-		*/
+<div class="row">
+	<div class="col-md-12 full-width">
+		<!-- BOX -->
+		<div class="box border custom-table">
+
+			<div class="box-title">
+				<h4><i class="fa fa-table"></i>List of all admin logs</h4>
+			</div>
+									
+
+			<div class="box-body" >
+			<?php
+				
+				$this->widget('zii.widgets.grid.CGridView', array(
+					'id'=>'datatables1',
+					'itemsCssClass'=>'datatable table table-striped table-bordered table-hover',
+					'dataProvider'=>$model->search(),
+					'filter'=>$model,'template'=>'{items}{summary}{pager}',
+					'pagerCssClass'=>'box-body',
+                	'pager'=>array(
+                        'header'=>'',
+                        'firstPageLabel'=>'&laquo;',
+                        'lastPageLabel'=>'&raquo;',
+                        'prevPageLabel'=>'<',
+                        'nextPageLabel'=>'>',
+                        'hiddenPageCssClass'=>'disabled',
+                        'selectedPageCssClass'=>'active',
+                        'htmlOptions'=>array(
+                            'class'=>'pagination',
+                        )
+                    ),
+                    'columns'=>array(
+			  			array('name'=>'id','htmlOptions'=>array('class'=>'center')),
+                           'username',
+							'ipaddress',
+							'logtime',
+							'controller',
+							'action',
+
+		
 		array(
 			'class'=>'CButtonColumn',
-		),
-	),
+			'header'=>'Operations',
+							'buttons'=>array(
+                                        'update'=>array(
+                                                        'visible'=>'true',
+                                                ),
+                                        'view'=>array(
+                                                        'visible'=>'true',
+                                                ),
+                                        'delete'=>array(
+                                                        'visible'=>'false',
+                                                ),
+                       						 )
+		     )
+	)
 )); ?>
+	</div>
+		</div>
+	<!-- /BOX -->
+	</div>
+</div>
