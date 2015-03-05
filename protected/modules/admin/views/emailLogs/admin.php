742055c34ada75860891e34326136e71ee5d69f9
AmbarishRai <raiambarish06@gmail.com>
2015-03-05 15:07:46 +0530

    EmailLogs

--- protected/modules/admin/views/emailLogs/admin.php
+++ protected/modules/admin/views/emailLogs/admin.php
@@ -25,25 +25,55 @@ $('.search-form form').submit(function(){
 });
 ");
 ?>
+<!-- PAGE HEADER-->
+<div class="row">
+    <div class="col-sm-12">
+        <div class="page-header">
+         	<h1>Manage Email Logs</h1>
 
-<h1>Manage Email Logs</h1>
-
+	       	</div>
+    </div>
+</div>
 <p>
 You may optionally enter a comparison operator (<b>&lt;</b>, <b>&lt;=</b>, <b>&gt;</b>, <b>&gt;=</b>, <b>&lt;&gt;</b>
 or <b>=</b>) at the beginning of each of your search values to specify how the comparison should be done.
 </p>
 
-<?php echo CHtml::link('Advanced Search','#',array('class'=>'search-button')); ?>
-<div class="search-form" style="display:none">
+<div class="search-form">
 <?php $this->renderPartial('_search',array(
 	'model'=>$model,
 )); ?>
 </div><!-- search-form -->
+<div class="row">
+	<div class="col-md-12 full-width">
+		<!-- BOX -->
+		<div class="box border custom-table">
+
+			<div class="box-title">
+				<h4><i class="fa fa-table"></i>List of all Email Logs</h4>
+			</div>
+									
+
+			<div class="box-body">
 
 <?php $this->widget('zii.widgets.grid.CGridView', array(
-	'id'=>'email-logs-grid',
-	'dataProvider'=>$model->search(),
-	'filter'=>$model,
+					'id'=>'datatables1',
+					'itemsCssClass'=>'datatable table table-striped table-bordered table-hover',
+					'dataProvider'=>$model->search(),
+					'filter'=>$model,'template'=>'{items}{summary}{pager}',
+					'pagerCssClass'=>'box-body',
+                	'pager'=>array(
+                        'header'=>'',
+                        'firstPageLabel'=>'&laquo;',
+                        'lastPageLabel'=>'&raquo;',
+                        'prevPageLabel'=>'<',
+                        'nextPageLabel'=>'>',
+                        'hiddenPageCssClass'=>'disabled',
+                        'selectedPageCssClass'=>'active',
+                        'htmlOptions'=>array(
+                            'class'=>'pagination',
+                        )
+                    ),
 	'columns'=>array(
 		'id',
 		'reciver',
@@ -61,3 +91,8 @@ or <b>=</b>) at the beginning of each of your search values to specify how the c
 		),
 	),
 )); ?>
+		</div>
+		</div>
+	<!-- /BOX -->
+	</div>
+</div>
