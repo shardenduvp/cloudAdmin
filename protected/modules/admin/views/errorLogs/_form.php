83937b73f364ce2b5ca14042414487fa2b2e212b
bhumika <shahbhumika.93@gmail.com>
2015-03-15 21:21:15 +0530

    error_logs

--- protected/modules/admin/views/errorLogs/_form.php
+++ protected/modules/admin/views/errorLogs/_form.php
@@ -4,7 +4,7 @@
 /* @var $form CActiveForm */
 ?>
 
-<div class="form">
+<div class="form-horizontal">
 
 <?php $form=$this->beginWidget('CActiveForm', array(
 	'id'=>'error-logs-form',
@@ -15,95 +15,156 @@
 	'enableAjaxValidation'=>false,
 )); ?>
 
-	<p class="note">Fields with <span class="required">*</span> are required.</p>
-
-	<?php echo $form->errorSummary($model); ?>
-
-	<div class="row">
+<div class="row">
+		<div class="col-md-12">
+			<div class="box border inverse mb0">
+				<div class="box-title">
+					<h4><i class="fa fa-search"></i>Update Email Logs</h4>
+				</div>
+			<div class="box-body big" >
+			<?php echo $form->errorSummary($model); ?>
+		   
+	<div class="form-group">
+		<div class="col-sm-4 tr-align">
 		<?php echo $form->labelEx($model,'user_type'); ?>
+		</div>
+		<div class="col-sm-6 col-offset-sm-2">
 		<?php echo $form->textField($model,'user_type',array('size'=>15,'maxlength'=>15)); ?>
 		<?php echo $form->error($model,'user_type'); ?>
+		</div>
 	</div>
 
-	<div class="row">
+<div class="form-group">
+		<div class="col-sm-4 tr-align">
 		<?php echo $form->labelEx($model,'user_name'); ?>
+		</div>
+		<div class="col-sm-6 col-offset-sm-2">
 		<?php echo $form->textArea($model,'user_name',array('rows'=>6, 'cols'=>50)); ?>
 		<?php echo $form->error($model,'user_name'); ?>
+		</div>
 	</div>
 
-	<div class="row">
+	<div class="form-group">
+		<div class="col-sm-4 tr-align">
 		<?php echo $form->labelEx($model,'error_code'); ?>
+		</div>
+		<div class="col-sm-6 col-offset-sm-2">
 		<?php echo $form->textField($model,'error_code'); ?>
 		<?php echo $form->error($model,'error_code'); ?>
+		</div>
 	</div>
 
-	<div class="row">
+	<div class="form-group">
+		<div class="col-sm-4 tr-align">
 		<?php echo $form->labelEx($model,'message'); ?>
+		</div>
+		<div class="col-sm-6 col-offset-sm-2">
 		<?php echo $form->textArea($model,'message',array('rows'=>6, 'cols'=>50)); ?>
 		<?php echo $form->error($model,'message'); ?>
+		</div>
 	</div>
 
-	<div class="row">
+	<div class="form-group">
+		<div class="col-sm-4 tr-align">
 		<?php echo $form->labelEx($model,'request_url'); ?>
+		</div>
+		<div class="col-sm-6 col-offset-sm-2">
 		<?php echo $form->textArea($model,'request_url',array('rows'=>6, 'cols'=>50)); ?>
 		<?php echo $form->error($model,'request_url'); ?>
+		</div>
 	</div>
 
-	<div class="row">
+	<div class="form-group">
+		<div class="col-sm-4 tr-align">
 		<?php echo $form->labelEx($model,'query_string'); ?>
+		</div>
+		<div class="col-sm-6 col-offset-sm-2">
 		<?php echo $form->textArea($model,'query_string',array('rows'=>6, 'cols'=>50)); ?>
 		<?php echo $form->error($model,'query_string'); ?>
+		</div>
 	</div>
 
-	<div class="row">
+	<div class="form-group">
+		<div class="col-sm-4 tr-align">
 		<?php echo $form->labelEx($model,'file_name'); ?>
+		</div>
+		<div class="col-sm-6 col-offset-sm-2">
 		<?php echo $form->textArea($model,'file_name',array('rows'=>6, 'cols'=>50)); ?>
 		<?php echo $form->error($model,'file_name'); ?>
+		</div>
 	</div>
 
-	<div class="row">
+	<div class="form-group">
+		<div class="col-sm-4 tr-align">
 		<?php echo $form->labelEx($model,'line_number'); ?>
+		</div>
+		<div class="col-sm-6 col-offset-sm-2">
 		<?php echo $form->textField($model,'line_number'); ?>
 		<?php echo $form->error($model,'line_number'); ?>
+		</div>
 	</div>
 
-	<div class="row">
+	<div class="form-group">
+		<div class="col-sm-4 tr-align">
 		<?php echo $form->labelEx($model,'error_type'); ?>
+		</div>
+		<div class="col-sm-6 col-offset-sm-2">
 		<?php echo $form->textField($model,'error_type',array('size'=>45,'maxlength'=>45)); ?>
 		<?php echo $form->error($model,'error_type'); ?>
+		</div>
 	</div>
 
-	<div class="row">
+	<div class="form-group">
+		<div class="col-sm-4 tr-align">
 		<?php echo $form->labelEx($model,'time'); ?>
+		</div>
+		<div class="col-sm-6 col-offset-sm-2">
 		<?php echo $form->textField($model,'time'); ?>
 		<?php echo $form->error($model,'time'); ?>
+		</div>
 	</div>
 
-	<div class="row">
+	<div class="form-group">
+		<div class="col-sm-4 tr-align">
 		<?php echo $form->labelEx($model,'reference_url'); ?>
+		</div>
+		<div class="col-sm-6 col-offset-sm-2">
 		<?php echo $form->textArea($model,'reference_url',array('rows'=>6, 'cols'=>50)); ?>
 		<?php echo $form->error($model,'reference_url'); ?>
+		</div>
 	</div>
 
-	<div class="row">
+	<div class="form-group">
+		<div class="col-sm-4 tr-align">
 		<?php echo $form->labelEx($model,'ipaddress'); ?>
+		</div>
+		<div class="col-sm-6 col-offset-sm-2">
 		<?php echo $form->textField($model,'ipaddress',array('size'=>45,'maxlength'=>45)); ?>
 		<?php echo $form->error($model,'ipaddress'); ?>
+		</div>
 	</div>
 
-	<div class="row">
+	<div class="form-group">
+		<div class="col-sm-4 tr-align">
 		<?php echo $form->labelEx($model,'browser'); ?>
+		</div>
+		<div class="col-sm-6 col-offset-sm-2">
 		<?php echo $form->textArea($model,'browser',array('rows'=>6, 'cols'=>50)); ?>
 		<?php echo $form->error($model,'browser'); ?>
+		</div>
+	</div>
+
+	<div class="form-group">
+		<div class="col-sm-4 tr-align">
+		<?php echo $form->labelEx($model,'user_id'); ?>
+		</div>
+		<div class="col-sm-6 col-offset-sm-2">
+		<?php echo $form->textField($model,'user_id'); ?>
+		<?php echo $form->error($model,'user_id'); ?>
+		</div>
 	</div>
 
 	<div class="row">
-		<?php echo $form->labelEx($model,'user_id'); ?>
-		<?php echo $form->textField($model,'user_id'); ?>
-		<?php echo $form->error($model,'user_id'); ?>
-	</div>
-
-	<div class="row buttons">
 		<?php echo CHtml::submitButton($model->isNewRecord ? 'Create' : 'Save'); ?>
 	</div>
 
