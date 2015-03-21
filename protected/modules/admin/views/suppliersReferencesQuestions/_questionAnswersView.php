88c3fa703115cd69264f0c00499758ed12358c9b
bhumika <shahbhumika.93@gmail.com>
2015-03-22 01:39:12 +0530

    SupplierReferenceQuestion

--- protected/modules/admin/views/suppliersReferencesQuestions/_questionAnswersView.php
+++ protected/modules/admin/views/suppliersReferencesQuestions/_questionAnswersView.php
@@ -1,12 +1,12 @@
 <div class="row" >
 
-<div class="textfont col-md-8">
+<div class="textfont col-md-10">
 	<?php
 		foreach ($model as $answer) 
 		{
 			echo "<b>".$answer->reviewQuestions->title."</b>"."<br />";?>
 	<?php 
-			echo CHtml::textfield('answer['.$answer->id.'][answer]',$answer->answers,array('class'=>'form-control'));  
+			echo CHtml::textfield('answer['.$answer->id.'][answer]',$answer->answers,array('class'=>'form-control'))."<br>";  
 			echo CHtml::hiddenField('answer['.$answer->id.'][id]',$answer->id);
 			echo CHtml::hiddenField('answer['.$answer->id.'][questionId]',$answer->review_questions_id);
 			echo CHtml::hiddenField('answer['.$answer->id.'][referencesId]',$answer->suppliers_has_references_id);
@@ -25,7 +25,7 @@
 			?>
 </div>
 
-<div class="col-md-2 rating-testimonial">
+<div class="col-md-1 rating-testimonial center col-md-offset-5">
 		<?php 
 			if($refcount!=0)
 			{
