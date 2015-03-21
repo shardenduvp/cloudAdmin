bc07dcd82cf37e3e16387a046ac93a575c6dc98b
bhumika <shahbhumika.93@gmail.com>
2015-03-21 15:23:06 +0530

    changes

--- protected/modules/admin/views/suppliersHasPortfolio/_portfolio.php
+++ protected/modules/admin/views/suppliersHasPortfolio/_portfolio.php
@@ -200,7 +200,7 @@
 
                         <!-- View Modal -->
                         <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-                          <div class="modal-dialog">
+                          <div class="modal-dialog modal-lg">
                             <div class="modal-content">
                               <div class="modal-header">
                                 <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
@@ -212,12 +212,12 @@
                               <div class="modal-body" id="answersBody">
                                 
                               </div>
-                              <div class="modal-footer">
+                               <div class="modal-footer">
                                 <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                               </div>
                             </div>
+                          </div>
                         </div>
-                    </div>
-                </div>
                 <!--btn 1 -->
 
 
--- protected/modules/admin/views/suppliersReferencesQuestions/_questionAnswersView.php
+++ protected/modules/admin/views/suppliersReferencesQuestions/_questionAnswersView.php
@@ -1,16 +1,17 @@
-
-
 <div class="row" >
-<div class="textfont col-md-8">
-	<?php
-		foreach ($model as $answer) 
-		{
-			echo "<b>".$answer->reviewQuestions->title."</b>"."<br />";?>
-	<?php echo CHtml::textfield('answer["'.$answer->id.'"]',$answer->answers,array('class'=>'form-control','disabled'=>'true'));  ?><br />
-	<?php	}
-	?>
-</div>
-<div class="col-md-4">
-</div>
 
-</div>
+	<div class="textfont col-md-8">
+		<?php
+			foreach ($model as $answer) 
+			{
+				echo "<b>".$answer->reviewQuestions->title."</b>"."<br />";?>
+			<?php echo CHtml::textfield('answer["'.$answer->id.'"]',$answer->answers,array('class'=>'form-control','disabled'=>'true'));  ?><br />
+			<?php	
+				}
+			?>
+	</div>
+
+	<div class="col-md-2">
+	</div>
+
+</div> 
