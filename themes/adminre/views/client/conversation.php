cf8bb84d59b1776676160da38c346d7e26369d32
namankareer <naman.kareer@venturepact.com>
2015-03-24 21:26:17 +0530

    removed

--- themes/adminre/views/client/conversation.php
+++
@@ -1,8 +0,0 @@
-<div>
-
-    <div class="mt20" style="margin-top:70px;">
-
-    </div>
-    <?php $this->Widget('WidgetChatController'); ?>
-
-</div>
--- themes/adminre/views/mails/new_user.php
+++
@@ -1,4 +0,0 @@
-New User registered . Here is the detail :<br />
-<b>ID : </b> <?php echo $id; ?><br />
-<b>Name : </b> <?php echo $name; ?><br />
-<b>Email :</b> <?php echo $email; ?>
