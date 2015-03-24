0eb2c6dd2e58c87a2e0ec6effb947bfc0d884bc7
namankareer <naman.kareer@venturepact.com>
2015-03-24 21:26:50 +0530

    remvoed

--- themes/adminre/views/mails/send_reminder.php
+++
@@ -1,9 +0,0 @@
-Hi <?php echo $data[1]['client_name']; ?>,<br /><br />
-
-Just a reminder that you have a pending invoice of Amount: $<?php echo $data[0]['amount'] ?> against the Module: <?php echo $data[0]['module'] ?> for the project <?php echo $data[1]['project_name']; ?>.
-<br /><br />
-Feel free to <a href="<?php echo Yii::app()->createAbsoluteUrl('site/call');?>">Schedule a call</a> if you need any assistance. <br /><br />
-
-
-All the best!<br />
-VenturePact Team	
